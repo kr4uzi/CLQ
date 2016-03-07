b792acf2e1428ae8c96f16997096a2d4a8e57275
kr4uzi <kr4uzi@gmail.com>
2016-03-07 07:53:56 +0100

    added user modifiable config file (autogenerated if no exisiting)

---
+++ CLQ/config.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <map>
+
+class config
+{
+public:
+	class parse_error
+	{
+	private:
+		std::string error_message;
+
+	public:
+		parse_error(const std::string& error_message)
+			: error_message(error_message)
+		{ }
+
+		std::string error_msg() const { return error_message; }
+		bool operator!() const { return error_message.empty(); }
+	};
+
+private:
+	std::map<std::string, std::string> cfg;
+
+public:
+	static parse_error parse(const std::string& filename, config& cfg);
+
+	bool exists(const std::string& key) const;
+
+	std::string get_string(const std::string& key, const std::string& default_value = "") const;
+	double get_double(const std::string& key, double default_value = 0) const;
+	unsigned get_unsinged(const std::string& key, unsigned default_value = 0) const;
+	signed get_signed(const std::string& key, signed default_value = 0) const;
+	bool get_bool(const std::string& key, bool default_value = false) const;
+};
--- CLQ/main.cpp
+++ CLQ/main.cpp
@@ -1,15 +1,46 @@
 #include "mouse.h"
 #include "keyboard.h"
+#include "config.h"
 #include <iostream>
+#include <fstream>
 
 int main()
-{	
-	auto interval = boost::posix_time::milliseconds(20);
+{
+	boost::posix_time::milliseconds interval(20);
 	auto button = mouse::button::RIGHT;
+	keyboard::keycode hotkey = 0x91;	// VK_SCROLL
+
+	config cfg;
+	auto parse_error = config::parse("clq.cfg", cfg);
+	if (!parse_error && cfg.exists("interval") && cfg.exists("button") && cfg.exists("hotkey"))
+	{
+		interval = boost::posix_time::milliseconds(cfg.get_unsinged("interval", 20));
+		button = (cfg.get_string("button", "right") == "right") ? mouse::button::RIGHT : mouse::button::LEFT;
+		hotkey = cfg.get_unsinged("hotkey", 0x91);
+	}
+	else
+	{
+		std::cout << "There has been an error parsing [clq.cfg]!" << std::endl;
+		std::cout << "Creating [clq.cfg] with default values." << std::endl;
+		std::cout << "Proceeding with default values." << std::endl;
+		std::cout << std::endl;
+
+		std::ofstream of("clq.cfg", std::ios::trunc);
+		of << "# click interval in miliseconds" << std::endl;
+		of << "interval = 20" << std::endl;
+		of << std::endl;
+		of << "# button to autoclick: left or right" << std::endl;
+		of << "button = right" << std::endl;
+		of << std::endl;
+		of << "# hotkey as virtual-key code - default hotkey is SCROLL (0x91)" << std::endl;
+		of << "# https://msdn.microsoft.com/library/windows/desktop/dd375731.aspx" << std::endl;
+		of << "hotkey = 0x91" << std::endl;
+	}
 
 	std::cout << "CLQ Autoclicker - for the best DotA 2 player, the world has ever fucking seen" << std::endl << std::endl;
-	std::cout << "Start by pressing [SCROLL]" << std::endl;
+	std::cout << "Start by pressing [" << keyboard::to_string(hotkey) << "]" << std::endl;
 	std::cout << "The click interval is [" << interval.total_milliseconds() << "] ms" << std::endl;
+	std::cout << "The [" << ((button == mouse::button::RIGHT) ? "right" : "left") << "] mouse button will be clicked" << std::endl;
 	std::cout << std::endl;
 
 	boost::asio::io_service io;
@@ -19,16 +50,16 @@ int main()
 	keyboard::get_instance()->set_scroll_lock(false);
 	keyboard::get_instance()->on_key_press.connect([&](keyboard::message msg, keyboard::keycode vkey)
 	{
-		if (msg == keyboard::message::KEYUP && vkey == VK_SCROLL)
+		if (msg == keyboard::message::KEYUP && vkey == 1)
 		{
 			if (m.is_clicking())
 			{
-				std::cout << "State: deactivated" << "\r";
+				std::cout << "State: clicking    " << "\r";
 				m.click_stop();
 			}
 			else
 			{
-				std::cout << "State: activated" << "\r";
+				std::cout << "State: not clicking" << "\r";
 				m.click_start();
 			}
 		}
